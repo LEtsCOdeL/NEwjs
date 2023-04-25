4b987d57ced5d45ae74a2c4c8777c980d431ba49
Aashna Prashar <prasharaashna@gmail.com>
2023-04-25 15:42:52 +0530

    fifth commit

--- index.js
+++ index.js
@@ -14,14 +14,14 @@ let config2 = {
 
 axios.request(config2)
 .then((response) => {
-     global.access_token = response.data.access_token
+     const access_token = response.data.access_token; // initialization of access_token
   console.log(access_token);
 })
 .catch((error) => {
   console.log(error);
 });
 
-const access_token = access_token;
+// const access_token = access_token;
 
 
 ////////////////
