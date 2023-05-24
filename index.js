5eaf68ea556d7f0afe67f98aa0a1de8e2d48e69e
Aashna Prashar <prasharaashna@gmail.com>
2023-05-24 13:54:31 +0530

    firsdst commit

--- index.js
+++ index.js
@@ -1,116 +1,157 @@
 const fs = require('fs');
 require('dotenv').config();
-const axios = require('axios');
 const express = require('express');
+const axios = require('axios');
 const bodyParser = require('body-parser');
 
-const idsArray = require('./idsArray.json');
+const app = express();
+app.use(bodyParser.json());
 
 let data = '';
 
+// Obtain Zoho access token
 let config = {
   method: 'post',
   maxBodyLength: Infinity,
-  url: 'https://accounts.zoho.in/oauth/v2/token?refresh_token=1000.4611192342ea904404138db35bde5122.5b90d014d9e855e979ef2b4d8720abbb&client_id=1000.QOWO0XMYJCMFER0LS7UGU3MKFRKJNI&client_secret=f1e42bd92714a2251ab1730acf64e9618ba370c99a&grant_type=refresh_token',
+  url: 'https://accounts.zoho.com/oauth/v2/token?refresh_token=1000.63eea01115414f39221f6d325e1d875b.e6e623bcf6b9b1d2629c14831c7b6484&client_id=1000.G73LKHN42126L4O4L6AGP0Y57B48UA&client_secret=b24d8b4b3a7fe61ca795fa59d29c28af2c3d578223&grant_type=refresh_token',
   headers: {},
   data: data
 };
 
 axios.request(config)
-  .then(async (response) => {
-    const access_token = response.data.access_token; // Define access_token here
-    console.log(JSON.stringify(access_token));
-    global.access_token = access_token;
-    console.log("access data =" + access_token);
-    console.log(idsArray);
+  .then((response) => {
+    const access_token = response.data.access_token;
+    let configg = {
+      method: 'get',
+      maxBodyLength: Infinity,
+      url: 'https://www.zohoapis.com/bigin/v1/Contacts',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + access_token
+      }
+    };
 
-    // Pass the access_token to the initialize function or any other functions that require it
-    await initialize(access_token);
+    return axios.request(configg);
+  })
+  .then((response) => {
+    const responsedata = JSON.parse(JSON.stringify(response.data));
+    const idsArray = responsedata.data.map(item => item.id);
+    console.log(idsArray);
+    fs.writeFile('idsArray.json', JSON.stringify(idsArray), 'utf8', (err) => {
+      if (err) {
+        console.error('Error writing idsArray to file:', err);
+      } else {
+        console.log('idsArray has been exported to idsArray.json');
+      }
+    });
   })
   .catch((error) => {
     console.log(error);
   });
 
-async function initialize(access_token) {
-  const { TOKEN, SERVER_URL } = process.env;
-  const TELEGRAM_API = `https://api.telegram.org/bot${TOKEN}`;
-  const URI = `/webhook/${TOKEN}`;
-  const WEBHOOK_URL = SERVER_URL + URI;
+// Rest of the code for Telegram webhook and Zoho Bigin integration
+const { TOKEN, SERVER_URL } = process.env;
+const TELEGRAM_API = `https://api.telegram.org/bot${TOKEN}`;
+const URI = `/webhook/${TOKEN}`;
+const WEBHOOK_URL = SERVER_URL + URI;
 
-  const app = express();
-  app.use(bodyParser.json());
+const init = async () => {
+  const res = await axios.get(`${TELEGRAM_API}/setWebhook?url=${WEBHOOK_URL}`);
+  console.log(res.data);
+};
 
-  const init = async () => {
-    const res = await axios.get(`${TELEGRAM_API}/setWebhook?url=${WEBHOOK_URL}`);
-    // console.log(res.data);
-    // const idsArray = await getContactIds();
-    // console.log(idsArray);
+app.post(URI, async (req, res) => {
+  console.log("response obj", req.body);
+  var content = "";
 
-    // Set up webhooks for each contact ID
-    for (let i = 0; i < idsArray.length; i++) {
-      const contactId = idsArray[i];
-      const contactWebhookUrl = `${WEBHOOK_URL}/contact/${contactId}`;
-      await delay(1000); // Delay 1 second between each request
-      await axios.get(`${TELEGRAM_API}/setWebhook?url=${contactWebhookUrl}`);
-      console.log(`Webhook set for contact ID ${contactId}`);
-    }
-  };
+  const group_name = req?.body?.message?.chat?.title;
+  const user_name = req?.body?.message?.from?.username;
+  const first_name = req?.body?.message?.from?.first_name;
+  const last_name = req?.body?.message?.from?.last_name;
+  const unixDate = req?.body?.message?.date;
+  const message = req?.body?.message?.text;
 
-  app.post(URI, async (req, res) => {
-    console.log("response obj", req.body);
-    let content = "";
+  // setting up dateTime into required format
+  const dateTimeObj = new Date(unixDate*1000);
+  const dateTimeString = dateTimeObj.toLocaleString('en-US');
+  console.log(dateTimeString);
 
-    const group_name = req?.body?.message?.chat?.title;
-    const user_name = req?.body?.message?.from?.username;
-    const first_name = req?.body?.message?.from?.first_name;
-    const last_name = req?.body?.message?.from?.last_name;
-    const unixDate = req?.body?.message?.date;
-    const message = req?.body?.message?.text;
+  const dateTimeArr = dateTimeString.split(",");
 
-    // setting up dateTime into required format
-    const dateTimeObj = new Date(unixDate * 1000);
-    const dateTimeString = dateTimeObj.toLocaleString('en-US');
-    const dateTimeArr = dateTimeString.split(',');
+  // setting up Date format
+  const dateArr = dateTimeArr[0].trim().split("/");
+  const date = dateArr[0];
+  const month = dateArr[1];
+  const year = dateArr[2];
+  const finalDate = `${month}-${date}-${year}`;
+  console.log("FinalDate is ", finalDate);
 
-    // setting up Date format
-    const date = dateTimeArr[0].trim();
-    const [month, day, year] = date.split("/");
-    const finalDate = `${year}-${month}-${day}`;
+  // setting up Time format
+  const timeString = dateTimeArr[1].trim();
+  const timeArr = timeString.split(" ");
+  const timeArr1 = timeArr[0].split(":");
+  timeArr1.pop();
+  const finalTime = `${timeArr1.join(":")}${timeArr[1]}`;
+  console.log("FinalTime is ", finalTime);
+  
 
-    // setting up Time format
-    const time = dateTimeArr[1].trim();
-    const [hour, minute, second] = time.split(":");
-    const finalTime = `${hour}:${minute}:${second}`;
+  if(user_name){
+    content =`${group_name} | ${user_name} | ${finalDate} ${finalTime} - ${message}`;
+  }
+  else if (typeof(last_name) === 'undefined' || last_name === null){
+    content =`${group_name} | ${first_name} | ${finalDate} ${finalTime} - ${message}`;
+  }
+  else{
+    content =`${group_name} | ${first_name} ${last_name} | ${finalDate} ${finalTime} - ${message}`;
+  }
 
-    if (user_name) {
-      content = `${user_name} (${first_name} ${last_name}) sent a message in ${group_name} group at ${finalTime} on ${finalDate}.\n\n`;
-    } else {
-      content = `${first_name} ${last_name} sent a message in ${group_name} group at ${finalTime} on ${finalDate}.\n\n`;
-    }
 
-    content += `Message: ${message}\n\n`;
+  // module.exports(data2);
+  // const chatId = req.body.message.chat.id
+  // const text = req.body.message.text
 
-    const fileContent = JSON.stringify(content);
-    fs.writeFile('messageData.json', fileContent, (err) => {
-      if (err) {
-        console.log('Error writing to file:', err);
-      } else {
-        console.log('Data written to file successfully!');
-      }
-    });
 
-    res.sendStatus(200);
-  });
 
+  // insert record in Zoho Bigin
+    // Iterate over each contact ID and perform insert operation
+    idsArray.forEach((contactId) => {
+ 
+        let data = JSON.stringify({
+          "data": [
+            {
+              "Note_Content": content
+            }
+          ]
+        });
+  
+        let config = {
+          method: 'POST',
+          maxBodyLength: Infinity,
+          url: `https://www.zohoapis.in/bigin/v1/Deals/${contactId}/Notes?`,
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + access_token
+          },
+          data: data
+        };
+  
+        axios.request(config)
+          .then((response) => {
+            console.log('Data successfully pushed to Bigin for contact ID:', contactId);
+          })
+          .catch((error) => {
+            console.log('Error inserting data for contact ID:', contactId, error);
+          });
+      });
+      return res.send();
+    })
+
+    // });
+  // end of insert record 
+
+
+
+app.listen(process.env.PORT || 10000, async () => {
+  console.log('🚀 app running on port', process.env.PORT || 10000);
   await init();
-
-  const port = process.env.PORT || 3000;
-  app.listen(port, () => {
-    console.log(`🚀 app running on port ${port}`);
-  });
-}
-
-// Delay function to introduce a delay between requests
-function delay(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
+});
