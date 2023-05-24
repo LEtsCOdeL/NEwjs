c2d9252d6758795093957d7fe050093de1f8e92e
Aashna Prashar <prasharaashna@gmail.com>
2023-05-24 10:23:22 +0530

    Jack commit

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+const fs = require('fs');
 require('dotenv').config();
-const express = require('express');
-const idsArray = require('./idsArray.json');
 const axios = require('axios');
+const express = require('express');
 const bodyParser = require('body-parser');
+
+const idsArray = require('./idsArray.json');
 
 let data = '';
 
@@ -10,45 +12,53 @@ let config = {
   method: 'post',
   maxBodyLength: Infinity,
   url: 'https://accounts.zoho.in/oauth/v2/token?refresh_token=1000.4611192342ea904404138db35bde5122.5b90d014d9e855e979ef2b4d8720abbb&client_id=1000.QOWO0XMYJCMFER0LS7UGU3MKFRKJNI&client_secret=f1e42bd92714a2251ab1730acf64e9618ba370c99a&grant_type=refresh_token',
-  headers: { 
-  },
-  data : data
+  headers: {},
+  data: data
 };
 
 axios.request(config)
-.then((response) => {
-  console.log(JSON.stringify(response.data.access_token));
-  global.access_token =response.data.access_token;
-  console.log("access data ="+ access_token);
+  .then(async (response) => {
+    const access_token = response.data.access_token; // Define access_token here
+    console.log(JSON.stringify(access_token));
+    global.access_token = access_token;
+    console.log("access data =" + access_token);
+    console.log(idsArray);
 
-  console.log(idsArray);
-  
-})
-.catch((error) => {
-  console.log(error);
-});
+    // Pass the access_token to the initialize function or any other functions that require it
+    await initialize(access_token);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
+async function initialize(access_token) {
+  const { TOKEN, SERVER_URL } = process.env;
+  const TELEGRAM_API = `https://api.telegram.org/bot${TOKEN}`;
+  const URI = `/webhook/${TOKEN}`;
+  const WEBHOOK_URL = SERVER_URL + URI;
 
-//////////////
-const { TOKEN, SERVER_URL } = process.env
-const TELEGRAM_API = `https://api.telegram.org/bot${TOKEN}`
-const URI = `/webhook/${TOKEN}`
-const WEBHOOK_URL = SERVER_URL + URI
+  const app = express();
+  app.use(bodyParser.json());
 
-const app = express()
-app.use(bodyParser.json())
-
-const init = async () => {
-    const res = await axios.get(`${TELEGRAM_API}/setWebhook?url=${WEBHOOK_URL}`)
+  const init = async () => {
+    const res = await axios.get(`${TELEGRAM_API}/setWebhook?url=${WEBHOOK_URL}`);
     // console.log(res.data);
     // const idsArray = await getContactIds();
     // console.log(idsArray);
-}
 
-app.post(URI, async (req, res) => {
+    // Set up webhooks for each contact ID
+    for (let i = 0; i < idsArray.length; i++) {
+      const contactId = idsArray[i];
+      const contactWebhookUrl = `${WEBHOOK_URL}/contact/${contactId}`;
+      await delay(1000); // Delay 1 second between each request
+      await axios.get(`${TELEGRAM_API}/setWebhook?url=${contactWebhookUrl}`);
+      console.log(`Webhook set for contact ID ${contactId}`);
+    }
+  };
 
+  app.post(URI, async (req, res) => {
     console.log("response obj", req.body);
-    var content = "";
+    let content = "";
 
     const group_name = req?.body?.message?.chat?.title;
     const user_name = req?.body?.message?.from?.username;
@@ -58,117 +68,49 @@ app.post(URI, async (req, res) => {
     const message = req?.body?.message?.text;
 
     // setting up dateTime into required format
-    const dateTimeObj = new Date(unixDate*1000);
+    const dateTimeObj = new Date(unixDate * 1000);
     const dateTimeString = dateTimeObj.toLocaleString('en-US');
-    console.log(dateTimeString);
-
-    const dateTimeArr = dateTimeString.split(",");
+    const dateTimeArr = dateTimeString.split(',');
 
     // setting up Date format
-    const dateArr = dateTimeArr[0].trim().split("/");
-    const date = dateArr[0];
-    const month = dateArr[1];
-    const year = dateArr[2];
-    const finalDate = `${month}-${date}-${year}`;
-    console.log("FinalDate is ", finalDate);
+    const date = dateTimeArr[0].trim();
+    const [month, day, year] = date.split("/");
+    const finalDate = `${year}-${month}-${day}`;
 
     // setting up Time format
-    const timeString = dateTimeArr[1].trim();
-    const timeArr = timeString.split(" ");
-    const timeArr1 = timeArr[0].split(":");
-    timeArr1.pop();
-    const finalTime = `${timeArr1.join(":")}${timeArr[1]}`;
-    console.log("FinalTime is ", finalTime);
-    
+    const time = dateTimeArr[1].trim();
+    const [hour, minute, second] = time.split(":");
+    const finalTime = `${hour}:${minute}:${second}`;
 
-    if(user_name){
-      content =`${group_name} | ${user_name} | ${finalDate} ${finalTime} - ${message}`;
-    }
-    else if (typeof(last_name) === 'undefined' || last_name === null){
-      content =`${group_name} | ${first_name} | ${finalDate} ${finalTime} - ${message}`;
-    }
-    else{
-      content =`${group_name} | ${first_name} ${last_name} | ${finalDate} ${finalTime} - ${message}`;
+    if (user_name) {
+      content = `${user_name} (${first_name} ${last_name}) sent a message in ${group_name} group at ${finalTime} on ${finalDate}.\n\n`;
+    } else {
+      content = `${first_name} ${last_name} sent a message in ${group_name} group at ${finalTime} on ${finalDate}.\n\n`;
     }
 
+    content += `Message: ${message}\n\n`;
 
-    // module.exports(data2);
-    // const chatId = req.body.message.chat.id
-    // const text = req.body.message.text
-
-
-
-    // insert record in Zoho Bigin
-
-
-    // const contact_id = "496297000000287176"; 
-    const contactt_id=idsArray;
-    console.log(contactt_id);
-    let data = JSON.stringify({
-      "data": [
-        {
-          "Note_Content": content
-        }
-      ]
+    const fileContent = JSON.stringify(content);
+    fs.writeFile('messageData.json', fileContent, (err) => {
+      if (err) {
+        console.log('Error writing to file:', err);
+      } else {
+        console.log('Data written to file successfully!');
+      }
     });
 
-    contactt_id.forEach(addatafunc);
+    res.sendStatus(200);
+  });
 
+  await init();
 
-    function addatafunc(contact_id){
-      let config = {
-        method: 'POST',
-        maxBodyLength: Infinity,
-        url: `https://www.zohoapis.in/bigin/v1/Deals/${contact_id}/Notes?`,
-        headers: { 
-          'Content-Type': 'application/json', 
-          'Authorization': 'Bearer ' + access_token
-        },
-        data : data
-      };
-      
-      axios.request(config)
-      .then((response) => {
-        // console.log(JSON.stringify(response.data));
-        console.log("Data successfully pushed to Bigin")
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  
+  const port = process.env.PORT || 3000;
+  app.listen(port, () => {
+    console.log(`🚀 app running on port ${port}`);
+  });
+}
 
-    }
-    // let config = {
-    //   method: 'POST',
-    //   maxBodyLength: Infinity,
-    //   url: `https://www.zohoapis.in/bigin/v1/Deals/${contact_id}/Notes?`,
-    //   headers: { 
-    //     'Content-Type': 'application/json', 
-    //     'Authorization': 'Bearer ' + access_token
-    //   },
-    //   data : data
-    // };
-    
-    // axios.request(config)
-    // .then((response) => {
-    //   // console.log(JSON.stringify(response.data));
-    //   console.log("Data successfully pushed to Bigin")
-    // })
-    // .catch((error) => {
-    //   console.log(error);
-    // });
-
-    // end of insert record 
-
-    // await axios.post(`${TELEGRAM_API}/sendMessage`, {
-    //     chat_id: chatId,
-    //     text: text
-    // })
-    return res.send()
-
-})
-
-app.listen(process.env.PORT || 10000, async () => {
-    console.log('🚀 app running on port', process.env.PORT || 10000)
-    await init()
-})
+// Delay function to introduce a delay between requests
+function delay(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
